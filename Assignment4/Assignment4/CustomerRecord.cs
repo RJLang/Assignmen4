02b9a5e8216873b1fd8cd412011bb871979593df
RJ Lang <rjlang@hotmail.com>
2019-02-24 18:06:15 -0800

    Initial Commit

---
+++ Assignment4/Assignment4/CustomerRecord.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment4
+{
+    class CustomerRecord
+    {
+        #region Variables
+        private string lastName;
+        private string firstName;
+        private double customerNumber;
+               
+
+        public string LastName
+        {
+            get { return lastName; }
+        }
+
+        public string FistName
+        {
+            get { return firstName; }
+        }
+        public double CustomerNumber
+        {
+            get { return customerNumber; }
+        }
+
+        #endregion
+
+
+        #region Constructor
+        public CustomerRecord (string last, string first, double num)
+        {
+            lastName = last;
+            firstName = first;
+            customerNumber = num;
+        }
+        #endregion
+
+        #region methods
+
+        public new string ToString()
+        {
+            return ($"{LastName}, {FistName}: {CustomerNumber}");
+        }
+        #endregion
+    }
+}
---
+++ Assignment4/Assignment4/StackElement.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment4
+{
+    class StackElement : IEnumerable<CustomerRecord>
+    {
+        private List<CustomerRecord> list = new List<CustomerRecord>();
+
+        public void Push(CustomerRecord record)
+        {
+            list.Add(record);
+        }
+
+        public void Pop()
+        {
+            int rows = list.Count;
+            if (rows > 0)
+            {
+                list.RemoveAt(rows -1);//last entry
+            }
+        }
+
+        public string Peek()
+        {
+            if (list.Count > 0)
+            {
+
+                return list[0].ToString();
+            }
+            else return null;
+        }
+
+        public int Count ()
+        {
+            return list.Count;
+        }
+
+
+        //IEnumerable<CustomerRecord> List = list.GetEnumerator();
+        //public IEnumerable<CustomerRecord> GetEnumerator()
+        //{
+        //    List<CustomerRecord> flip = list.Reverse;
+        //}
+        //public IEnumerable GetEnumerator()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator<CustomerRecord> IEnumerable<CustomerRecord>.GetEnumerator()
+        {
+            //IEnumerator<CustomerRecord> ListEnum;
+
+            //foreach (CustomerRecord entry in list)
+            //{
+            //    ListEnum.A entry;
+            //}
+
+
+            //return ListEnum;
+            return ((IEnumerable<CustomerRecord>)list).GetEnumerator();
+        }
+    }
+}
